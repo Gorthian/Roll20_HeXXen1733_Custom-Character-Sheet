b04464efff6ef2ff7155b176a13275dd628ff652
Gorthian <74892068+Gorthian@users.noreply.github.com>
2021-02-08 11:35:23 +0100

    TokenMarker
    Erklärung für eigene Token Marker

--- api/TokenMarker.js
+++ api/TokenMarker.js
@@ -1,7 +1,7 @@
 /* 
 	EDITED by 			Gorthian
-	Version				1.0
-	Letzte Änderung		2021-01-25
+	Version				2.0
+	Letzte Änderung		2021-02-08
 	
 	GitHub				https://github.com/Gorthian/HeXXen1733_Custom_Character_Sheet
 	Wiki				https://github.com/Gorthian/HeXXen1733_Custom_Character_Sheet/wiki
@@ -9,6 +9,23 @@
 	
 	Dieses API-Skript zeigt Einflüsse und die aktuellen Puffer-LeP als Token Marker an
 */
+
+/*
+ * KONFIGURATION
+ *
+ * Um andere Token Marker zu verwenden genügt es in den folgenden Zeilen den entsprechenden Namen der Marker zu ändern.
+ * Den Namen einzelner Marker kann man ablesen indem in Roll20 mit der Maus über den gewünschten Marker fährt.
+ * Dem Namen muss dann noch ein "status_" vorangestellt werden.
+ *
+ * Möchte man eigene Marker verwenden muss man sich nach Einfügen dieses Skripts im Chat über den Befehl !markernames alle Marker auflisten lassen, und sich ID und Namen des gewünschten Markers notieren.
+ * Dieser muss dann in der Form ID::Name hier im Skript eingetragen werden.
+*/
+
+/* Puffer LeP */		var healthBuffer = "status_green";
+/* Äußerer Schaden */	var externalDamage = "status_arrowed";
+/* Innerer Schaden */	var internalDamage = "status_drink-me";
+/* Malusstufen */		var malus = "status_broken-skull";
+/* Lähmung */			var paralysis = "status_snail";
 
 function setMarker(tokens, status, value) {
     for (let i = 0; i < tokens.length; i++) {
@@ -25,22 +42,65 @@ on("change:attribute", function(obj) {
     var tokens = findObjs({type:'graphic', represents: obj.get("_characterid")});
 
     if (attr == "HealthBuffer") {
-        setMarker(tokens,"status_green",parseInt(value))
+        setMarker(tokens,healthBuffer,parseInt(value))
     }
     
     if (attr == "influenceExternalDamageLevel") {
-        setMarker(tokens,"status_arrowed",value)
+        setMarker(tokens,externalDamage,value)
     }
     
     if (attr == "influenceInternalDamageLevel") {
-        setMarker(tokens,"status_drink-me",value)
+        setMarker(tokens,internalDamage,value)
     }
     
     if (attr == "influenceMalusLevel") {
-        setMarker(tokens,"status_broken-skull",value)
+        setMarker(tokens,malus,value)
     }
     
     if (attr == "influenceParalysisLevel") {
-        setMarker(tokens,"status_snail",value)
+        setMarker(tokens,paralysis,value)
     }
+});
+
+/* Standardfunktionen der API-Skript
+ * Siehe: https://wiki.roll20.net/API:Token_Markers
+ */
+on("ready", () => {
+    const tokenMarkers = JSON.parse(Campaign().get("token_markers"));
+    const getChatMessageFromTokenMarkers = markers => {
+        let chatMessage = '';
+        _.each(markers, marker => {
+            chatMessage += `<p><img src='${marker.url}'> ${marker.id}: ${marker.name}</p>`;
+        });
+        return chatMessage;
+    };
+on("chat:message", msg => {
+        if(msg.content.split(" ")[0].toLowerCase() === '!markernames') {
+            let chatMessage = getChatMessageFromTokenMarkers(tokenMarkers);
+            sendChat("Token Markers", chatMessage);
+        } else if(msg.content.split(" ")[0].toLowerCase() === '!markerids') {
+            const markerName = msg.content.split(" ")[1].toLowerCase();
+            let results = [];
+            _.each(tokenMarkers, marker => {
+                if(marker.name.toLowerCase() === markerName) results.push(marker);
+            });
+            log(results);
+            let chatMessage = getChatMessageFromTokenMarkers(results);
+            chatMessage = chatMessage || 'Unable to find any matching token markers'
+            sendChat("Token Markers", chatMessage);
+        } else if(msg.content.split(" ")[0].toLowerCase() === '!settokenmarker') {
+            const markerName = msg.content.split(" ")[1].toLowerCase();
+            if (!msg.selected && msg.selected[0]._type == "graphic") return;
+            obj = getObj(msg.selected[0]._type, msg.selected[0]._id);
+            currentMarkers = obj.get("statusmarkers").split(',');
+            currentMarkers.push(markerName);
+            obj.set("statusmarkers", currentMarkers.join(','));
+        } else if(msg.content.split(" ")[0].toLowerCase() === '!gettokenmarkers') {
+            if (!msg.selected) return;
+            if (msg.selected[0]._type !== "graphic") return;
+            obj = getObj(msg.selected[0]._type, msg.selected[0]._id);
+            currentMarkers = obj.get("statusmarkers");
+            sendChat("Token Markers", currentMarkers);
+        }
+    });
 });
